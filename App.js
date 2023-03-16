6307d7931af4792e3915d80b3b0739c64115f35d
paya-paya <paya_aiko_416y@icloud.com>
2023-03-16 20:02:50 +0900

    ポートの接続の変更

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const route = require("./routes/cheer");
+require("dotenv").config();
 
 app.use(express.json())
 app.use(express.static("./public"));
@@ -9,7 +10,7 @@ app.use("/api/v1/cheer", route);
 
 console.log('start')
 
-app.listen(5000);
+app.listen(process.env.PORT || 5000);
 
 // const server = app.listen(5000, function(){
 //     console.log("Node.js is listening to PORT:" + server.address().port);
